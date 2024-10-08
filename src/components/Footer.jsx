7d0e3d7c8d24ac31548558639c8ec7984aa94385
Atul Kumar <919atul@gmail.com>
2024-10-08 19:51:58 +0530

    dynamic footer date added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React  from "react";
 function Footer(){
     const date=new Date();
-    console.log(date.getFullYear());
     return(
         <div className="b-0 py-4 bg-gray-100 rounded-b-2xl text-center dark:bg-gray-800">
             © {date.getFullYear()} All Rights Reserved by <span className="text-red-400">Atul Kumar</span>.
